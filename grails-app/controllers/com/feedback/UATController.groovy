36b00216f5e5eee3c9de54c0edd47dea246800fa
Aaron <aa@unkown.com>
2020-02-28 21:39:19 -0500

    Fix for the workflow

--- grails-app/controllers/com/feedback/UATController.groovy
+++ grails-app/controllers/com/feedback/UATController.groovy
@@ -3,6 +3,7 @@ package com.feedback
 import grails.converters.JSON
 import grails.plugin.springsecurity.SpringSecurityService
 import grails.plugin.springsecurity.annotation.Secured
+import org.apache.poi.ss.formula.functions.T
 
 @Secured(['IS_AUTHENTICATED_FULLY'])
 class UATController {
@@ -15,16 +16,13 @@ class UATController {
         User me = springSecurityService.getCurrentUser()
         //Make sure it exists and this user has access
         session["uatSession"]["userId"] = me.id
-        def res = [response:"Success"]
-        withFormat {
-            '*' { render res as JSON }
-        }
+        redirect(action:"myQuestionAndAnswers")
     }
 
-    def getQuestionAndAnswers() {
+    def myQuestionAndAnswers() {
         def res = [:]
         if(session["uatSession"]?.uatId) {
-            res = UATService.getUATQuestions(springSecurityService.getCurrentUser(),session["uatSession"]?.uatId)
+            res = UATService.getUATQuestions(springSecurityService.getCurrentUser(),session["uatSession"].uatId)
         } else {
             res = session["uatSession"]
         }
